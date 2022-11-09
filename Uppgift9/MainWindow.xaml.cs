3934ff5ee2e151d0ffd6926b9e12fd82cddbd057
Wilhelm J <feja2102@student.miun.se>
2022-11-09 15:41:32 +0100

    Uppgift 9 fixa

---
+++ Uppgift9/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void konBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(galBox.Text) && string.IsNullOrEmpty(sikBox.Text) && string.IsNullOrEmpty(knutBox.Text))
+            {
+                MessageBox.Show("Du måste ange ett värde");
+                return;
+            }
+            
+            int gal = int.Parse(galBox.Text);
+            int sik = int.Parse(sikBox.Text);
+            int knut = int.Parse(knutBox.Text);
+
+
+            int gal1 = gal * 0;
+            int sik1 = sik * 17;
+            int knut1 = knut * 493;
+
+            int sum = gal1 + sik1 + knut1;
+
+
+            
+
+
+
+
+            summa.Content = sum.ToString(format: $"{Math.Round(sum * 62.02, 0 ) + " SEK"}");
+
+
+
+            //summa.Content = sum.ToString(sum + " SEK");
+        }
+    }
+}
