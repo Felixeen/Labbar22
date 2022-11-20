028e174d925ce67bc9156f1b68f99232acc8a7e3
Wilhelm J <feja2102@student.miun.se>
2022-11-20 17:12:37 +0100

    Uppgift 17 klar

---
+++ Uppgift17/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void NumberOfVowels()
+        {
+            string mening = textBox.Text;
+            int numberOfVowels = 0;
+            for (int i = 0; i < mening.Length; i++)
+            {
+                if (mening[i] == 'a' || mening[i] == 'e' || mening[i] == 'i' || mening[i] == 'o' || mening[i] == 'u')
+                {
+                    numberOfVowels++;
+                }
+            }
+            vowelsBlock.Text = "Antal vokaler: " + numberOfVowels.ToString();
+
+        }
+
+
+        private void Jibberish()
+        {
+            string mening = textBox.Text;
+            for (int i = 0; i < mening.Length; i++)
+            {
+                if (mening[i] == 'a' || mening[i] == 'e' || mening[i] == 'i' || mening[i] == 'o' || mening[i] == 'u')
+                {
+                    mening = mening.Replace(mening[i], 'ö');
+                }
+                
+                else if (mening[i] == 'A' || mening[i] == 'E' || mening[i] == 'I' || mening[i] == 'O' || mening[i] == 'U')
+                {
+                    mening = mening.Replace(mening[i], 'Ö');
+                }
+            }
+            resultBlock.Text = mening.ToString();
+
+        }
+        
+        
+
+
+        
+        
+    private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NumberOfVowels();
+            Jibberish();
+
+        }
+        
+        
+        
+    }
+
+    
+
+    
+}
