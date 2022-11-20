9cb8dc5b719f51e6999cd9faf6a18a9b3f7a9c83
Wilhelm J <feja2102@student.miun.se>
2022-11-20 16:05:02 +0100

    Uppgift 16 klar

---
+++ Uppgift16/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uppgift16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int birthDate = int.Parse(yearBox.Text);
+
+            int now = int.Parse(DateTime.Now.ToString("yyyy"));
+            int dob = birthDate;
+            int age = now - dob;
+
+            MessageBox.Show("Du är " + age + " år gammal");
+
+            
+
+
+
+        }
+    }
+
+}
+    
